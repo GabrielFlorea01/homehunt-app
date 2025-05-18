4edb839a1cfdc0008b9cf465b12c8d55613736ad
Gabi <gabriel.florea01@gmail.com>
2025-05-18 18:28:08 +0300

    wip

--- lib/pages/edit_listing_page.dart
+++ lib/pages/edit_listing_page.dart
@@ -1,9 +1,7 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:homehunt/error_widgets/error_banner.dart';
 
