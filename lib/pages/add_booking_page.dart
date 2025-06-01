31f5675dc8a7b8609eea75407d7fed01709a39ea
Gabi <gabriel.florea01@gmail.com>
2025-06-01 16:05:49 +0300

    wip

--- lib/pages/add_booking_page.dart
+++ lib/pages/add_booking_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:homehunt/pages/my_bookings_page.dart';
 
 class AddBookingPage extends StatefulWidget {
   final String agentId;
